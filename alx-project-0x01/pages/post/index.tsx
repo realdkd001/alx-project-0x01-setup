3c654083bfe113b07387445be1b1cfd7a37e40c3
Dzrekey001 <dkdzrekey001@gmail.com>
2025-09-16 14:08:53 +0000

    feat: setting up

---
+++ alx-project-0x01/pages/post/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const Index = () => {
+  return (
+    <div>Posts</div>
+  )
+}
+
+
+export default Index
---
+++ alx-project-0x01/pages/users/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const Index = () => {
+  return (
+    <div>Users</div>
+  )
+}
+
+
+export default Index
